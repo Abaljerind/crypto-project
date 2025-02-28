d766116c6c97010eefa974abe2146724e332f4dc
Abaljerind <abaljerinda@gmail.com>
2025-02-28 21:06:57 +0700

    Create component SignIn and give it styles
    Create title, form for email, password and button
    Create a link to go to component SignUp
    And use icons from react-icons for email and password

---
+++ src/routes/SignIn.jsx
@@ -0,0 +1,47 @@
+import { AiFillLock, AiOutlineMail } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
+const SignIn = () => {
+  return (
+    <div>
+      <div className="mx-auto min-h-[600px] max-w-[400px] px-4 py-20">
+        <h1 className="text-2xl font-bold">Sign In</h1>
+        <form>
+          <div className="my-4">
+            <label>Email</label>
+            <div className="relative my-2 w-full rounded-2xl shadow-xl">
+              <input
+                className="bg-bg-primary border-bg-input w-full rounded-2xl border p-2"
+                type="email"
+              />
+              <AiOutlineMail className="absolute top-3 right-2 text-gray-400" />
+            </div>
+          </div>
+
+          <div className="my-4">
+            <label>Password</label>
+            <div className="relative my-2 w-full rounded-2xl shadow-xl">
+              <input
+                className="bg-bg-primary border-bg-input w-full rounded-2xl border p-2"
+                type="password"
+              />
+              <AiFillLock className="absolute top-3 right-2 text-gray-400" />
+            </div>
+          </div>
+
+          <button className="bg-bg-button text-bg-input my-2 w-full rounded-2xl p-3 shadow-xl">
+            Sign In
+          </button>
+        </form>
+        <p className="my-4">
+          Don't have an account?{" "}
+          <Link to={"/signup"} className="text-text-accent">
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
