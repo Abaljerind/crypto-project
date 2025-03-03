ad49e892286a3117f0f89d6c35d7541365c23f64
Abaljerind <abaljerinda@gmail.com>
2025-03-03 17:41:51 +0700

    Add a function for update data from firebase
    Create and use useState to save favoite coin
    Create and import user from UserAuth()
    Create coinPath variable to access data coin saved in firestore
    Create function saveCoin with async and await to update data of favorite coin of a user if user already login
    If user hasn't login then give it an alert
    Add event onClick to tag td
    Use ternary operator to check state savedCoin to change star icon with fill or outline one

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -1,12 +1,43 @@
-import { AiOutlineStar } from "react-icons/ai";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { Sparklines, SparklinesLine } from "react-sparklines";
+import { UserAuth } from "../context/AuthContext";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { useState } from "react";
 
 const CoinItem = ({ coin }) => {
+  // state untuk mengganti icon star nya.
+  const [savedCoin, setSavedCoin] = useState(false);
+  const { user } = UserAuth();
+
+  // file path untuk mengakses data (coin) yang disimpan secara spesifik data nya berdasarkan user email yang login
+  const coinPath = doc(db, "users", `${user?.email}`);
+
+  // function handle untuk menyimpan dan mengirim data (coin) ke firestore sesuai email yang digunakan
+  // jadi tidak akan tertukar data watchlist nya dengan user lain
+  const saveCoin = async () => {
+    if (user?.email) {
+      setSavedCoin(true);
+      // updateDoc untuk mengupdate data (coin) berdasarkan coinPath nya
+      await updateDoc(coinPath, {
+        watchList: arrayUnion({
+          id: coin.id,
+          name: coin.name,
+          image: coin.image,
+          rank: coin.market_cap_rank,
+          symbol: coin.symbol,
+        }),
+      });
+    } else {
+      alert("Please sign in to save a coin to your watch list");
+    }
+  };
+
   return (
     <tr className="h-20 overflow-hidden border-b">
-      <td>
-        <AiOutlineStar />
+      <td onClick={saveCoin}>
+        {savedCoin ? <AiFillStar /> : <AiOutlineStar />}
       </td>
       <td>{coin.market_cap_rank}</td>
       <td>
