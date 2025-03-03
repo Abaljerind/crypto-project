94ff82e9442c0f58ee92f39cfcf9cbd50e43a863
Abaljerind <abaljerinda@gmail.com>
2025-03-03 16:21:52 +0700

    Create state for coins to save data from API
    Create variable url contains url of API from coingecko
    Use useEffect to fetch data using axios from url
    Wrap Navbar and Routes tag with ThemeProvider and AuthContextProvider
    Wrap Route tag with element consist of Home, Account, SignIn, SignUp and CoinPage with detail page of every coin

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./context/ThemeContext";
+import Home from "./routes/Home";
+import Account from "./routes/Account";
+import SignIn from "./routes/SignIn";
+import SignUp from "./routes/SignUp";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CoinPage from "./components/CoinPage";
+import Footer from "./components/Footer";
+import AuthContextProvider from "./context/AuthContext";
+
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&sparkline=true&price_change_percentage=24";
+
+  useEffect(() => {
+    axios
+      .request(url)
+      .then((res) => setCoins(res.data))
+      .catch((err) => console.error(err));
+  }, [url]);
+
   return (
-    <>
-      <h1 class="text-text-secondary text-md font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    // ThemeProvider memuat style untuk tema window nya, yang secara otomatis mengganti tema nya sesuai keinginan kita
+    <ThemeProvider>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home coins={coins} />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/coin/:coinId" element={<CoinPage />}>
+            <Route path=":coinId" />
+          </Route>
+        </Routes>
+        <Footer />
+      </AuthContextProvider>
+    </ThemeProvider>
   );
 }
 
