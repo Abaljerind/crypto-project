197d81a102f95eaaef5fe13badb698c226250a7a
Abaljerind <abaljerinda@gmail.com>
2025-03-03 16:26:16 +0700

    Create and use 3 useState for email, password and error Create variable navigate Create variable signUp from UserAuth() Create function handleSubmit with async await for user login and redirect them to Home page if it success Add event onSubmit to form Add event onChange to input email and password

--- src/routes/SignIn.jsx
+++ src/routes/SignIn.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
 import { AiFillLock, AiOutlineMail } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { signIn } = UserAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signIn(email, password);
+      navigate("/");
+    } catch (e) {
+      setError(e.message);
+      console.log(e.message);
+    }
+  };
+
   return (
     <div>
       <div className="mx-auto min-h-[600px] max-w-[400px] px-4 py-20">
         <h1 className="text-2xl font-bold">Sign In</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="my-4">
             <label>Email</label>
             <div className="relative my-2 w-full rounded-2xl shadow-xl">
               <input
+                onChange={(e) => setEmail(e.target.value)}
                 className="bg-bg-primary border-bg-input w-full rounded-2xl border p-2"
                 type="email"
               />
@@ -22,6 +43,7 @@ const SignIn = () => {
             <label>Password</label>
             <div className="relative my-2 w-full rounded-2xl shadow-xl">
               <input
+                onChange={(e) => setPassword(e.target.value)}
                 className="bg-bg-primary border-bg-input w-full rounded-2xl border p-2"
                 type="password"
               />
