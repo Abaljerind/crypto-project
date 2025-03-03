b92d8d3e2abcdf9346241b53c766c894341564d9
Abaljerind <abaljerinda@gmail.com>
2025-03-03 17:37:21 +0700

    Add a logout function from firebase
    Create and import user and logout from UserAuth()
    Create navigate with useNavigate() from react-router-dom
    Create function handleSignOut for user logout and redirect them to page Home
    Check user email if it's true then show menu Account and Sign Out, if it's false then show button Sign In and Sign Up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { useState } from "react";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
 
+  const { user, logout } = UserAuth();
+  const navigate = useNavigate();
+
   const handleNav = () => {
     setNav(!nav);
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await logout();
+      navigate("/");
+    } catch (e) {
+      console.log(e.message);
+    }
   };
 
   return (
@@ -18,17 +31,34 @@ const Navbar = () => {
       <div className="hidden md:block">
         <ThemeToggle />
       </div>
-      <div className="hidden md:block">
-        <Link to={"/signin"} className="hover:text-text-accent p-4">
-          Sign In
-        </Link>
-        <Link
-          to={"/singup"}
-          className="bg-bg-button text-bg-input ml-2 rounded-2xl px-5 py-2 shadow-lg hover:shadow-2xl"
-        >
-          Sign Up
-        </Link>
-      </div>
+
+      {/* cek apakah user sudah login atau belum berdasarkan email nya dari UserAuth(); 
+      untuk mengganti Sign In dan Sign Up nya.  */}
+      {user?.email ? (
+        <div className="md:flex">
+          <Link to={"/account"} className="hidden p-4 md:block">
+            Account
+          </Link>
+          <button
+            className="hidden cursor-pointer md:block"
+            onClick={handleSignOut}
+          >
+            Sign Out
+          </button>
+        </div>
+      ) : (
+        <div className="hidden md:block">
+          <Link to={"/signin"} className="hover:text-text-accent p-4">
+            Sign In
+          </Link>
+          <Link
+            to={"/signup"}
+            className="bg-bg-button text-bg-input ml-2 rounded-2xl px-5 py-2 shadow-lg hover:shadow-2xl"
+          >
+            Sign Up
+          </Link>
+        </div>
+      )}
 
       {/* Menu Icon */}
       <div onClick={handleNav} className="z-10 block cursor-pointer md:hidden">
@@ -44,11 +74,11 @@ const Navbar = () => {
         }
       >
         <ul className="w-full p-4">
-          <li className="border-b py-6">
+          <li onClick={handleNav} className="border-b py-6">
             <Link to={"/"}>Home</Link>
           </li>
-          <li className="border-b py-6">
-            <Link to={"/"}>Account</Link>
+          <li onClick={handleNav} className="border-b py-6">
+            <Link to={"/account"}>Account</Link>
           </li>
           <li className="py-6">
             <ThemeToggle />
@@ -56,12 +86,18 @@ const Navbar = () => {
         </ul>
         <div className="flex w-full flex-col p-4">
           <Link to={"/signin"}>
-            <button className="bg-bg-primary text-text-primary border-bg-secondary my-2 w-full rounded-2xl border p-3 shadow-xl">
+            <button
+              onClick={handleNav}
+              className="bg-bg-primary text-text-primary border-bg-secondary my-2 w-full cursor-pointer rounded-2xl border p-3 shadow-xl"
+            >
               Sign In
             </button>
           </Link>
           <Link to={"/signup"}>
-            <button className="bg-bg-button text-bg-input my-2 w-full rounded-2xl p-3 shadow-xl">
+            <button
+              onClick={handleNav}
+              className="bg-bg-button text-bg-input my-2 w-full cursor-pointer rounded-2xl p-3 shadow-xl"
+            >
               Sign Up
             </button>
           </Link>
